e91aea92e45fc882b79e04bb968dd733f0a435f0
Igor Britto <igor.galvao@gmail.com>
2020-11-06 11:46:20 +0000

    Adds passing test

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -4,4 +4,11 @@ class GroupTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "accepts owner association" do
+    owner = Customer.create
+    network = Network.create(owner: owner)
+    group = Group.create(network: network)
+    
+    assert(group.owner == owner)
+  end
 end
